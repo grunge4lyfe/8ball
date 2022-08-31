15ef58ce5c827d643b1a82c7b8b4324dfc792f84
DelightedCat <git@delightedcat.net>
2022-08-31 19:32:36 +0200

    Remove blue liquid easter egg

--- 8ball.c
+++ 8ball.c
@@ -30,10 +30,6 @@ int main(int argc, char *argv[]) {
 
     if (argc < 2) {
         printf("usage: %s \"<your question here>\"\n", argv[0]);
-
-        if (rand() % 10 == 0)  {
-            printf("warning: do not drink the blue liquid inside! ;)\n");
-        }
         return -1;
     }
 
